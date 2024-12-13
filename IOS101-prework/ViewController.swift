42c99c7e06aa9e1b092a9fc17f2fa363fd5b3c2e
GoldenBadger <roymanclee@gmail.com>
2024-12-13 06:28:22 -0600

    Initial Commit

---
+++ IOS101-prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS101-prework
+//
+//  Created by 李弘政 on 2024/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
